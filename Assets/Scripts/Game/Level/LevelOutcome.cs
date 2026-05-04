6945e3fd3591a97b3f130a1830de9d7a0c552a34
amadeus <piskunovicekaterina@gmail.com>
2026-05-04 16:12:01 +0200

    PJM-33: #done #comment enviroment added

---
+++ Assets/Scripts/Game/Level/LevelOutcome.cs
@@ -0,0 +1,26 @@
+using Core;
+using UnityEngine;
+using VContainer;
+
+public class LevelOutcome : MonoBehaviour
+{
+    [Inject] private Player player;
+    [Inject] private IEnemyCounter enemyCounter;
+    [Inject] private PlayerInput playerInput;
+    private StateMachine _fsm;
+
+    private void Awake()
+    {
+        _fsm = new StateMachine(this);
+    }
+
+    private void Start()
+    {
+        var playingState = new PlayingState(playerInput);
+        var winState = new WinState();
+        var loseState = new LoseState();
+        _fsm.AddTransition(playingState, winState, () => enemyCounter.Count == 0);
+        _fsm.AddTransition(playingState, loseState,  () => player.Stats.Get<Health>().IsDead.Value);
+        _fsm.Start(playingState);
+    }
+}
---
+++ Assets/Scripts/Game/Level/States/WinState.cs
@@ -0,0 +1,24 @@
+using Core;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinState : State
+{
+    
+    public WinState()
+    {
+        
+    }
+
+    protected override void OnEnter()
+    {
+    }
+
+
+    private void NextLevel()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex;
+        int nextIndex = ++index % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextIndex);
+    }
+}
